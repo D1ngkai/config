1a811bd34640803b2a5e2ba35cb0653c407c41d0
xyp9x <xyp9x@gmail.com>
2019-05-08 17:43:35 +0800

    linux ex2

---
+++ MazeDFS1.c
@@ -0,0 +1,38 @@
+    total 156
+    drwx------ 17 xyp9x xyp9x  4096  5月  8 17:18 .
+    r--r--  1 xyp9x xyp9x    57  1月 13 23:45 .bash_profile
+    -rw-r--r--  1 xyp9x xyp9x  3977  1月 13 23:45 .bashrc
+    drwx------ 10 xyp9x xyp9x  4096  5月  8 16:34 .cache
+drwxr-xr-x 17 xyp9x xyp9x  4096  5月  7 23:07 .config
+drwxr-xr-x  3 xyp9x xyp9x  4096  5月  8 17:10 config
+drwxr-xr-x  2 xyp9x xyp9x  4096  5月  7 22:29 Desktop
+drwxr-xr-x  2 xyp9x xyp9x  4096  5月  7 22:37 dir
+-rw-r--r--  1 xyp9x xyp9x  4855 10月 30  2017 .dir_colors
+drwxr-xr-x  2 xyp9x xyp9x  4096  5月  7 22:29 Documents
+drwxr-xr-x  2 xyp9x xyp9x  4096  5月  7 22:29 Downloads
+-rw-------  1 xyp9x xyp9x    16  5月  7 22:29 .esd_auth
+-rw-r--r--  1 xyp9x xyp9x   494 10月  5  2018 .face
+-rw-r--r--  1 xyp9x xyp9x    46  5月  8 17:12 .gitconfig
+-rw-------  1 xyp9x xyp9x   668  5月  8 17:05 .ICEauthority
+drwxr-xr-x  3 xyp9x xyp9x  4096  3月 12 18:18 .local
+drwxr-xr-x  2 xyp9x xyp9x  4096  5月  7 22:29 Music
+drwxr-xr-x  2 xyp9x xyp9x  4096  5月  7 22:29 Pictures
+-rw-r--r--  1 xyp9x xyp9x    64 11月 29 05:54 .profile
+drwxr-xr-x  2 xyp9x xyp9x  4096  5月  7 22:29 Public
+drwx------  2 xyp9x xyp9x  4096  5月  8 17:20 .ssh
+drwxr-xr-x  2 xyp9x xyp9x  4096  5月  7 22:29 Templates
+-rw-r-----  1 xyp9x xyp9x     4  5月  8 17:05 .vboxclient-clipboard.pid
+-rw-r-----  1 xyp9x xyp9x     4  5月  8 17:05 .vboxclient-display.pid
+-rw-r-----  1 xyp9x xyp9x     4  5月  8 17:05 .vboxclient-draganddrop.pid
+-rw-r-----  1 xyp9x xyp9x     4  5月  8 17:05 .vboxclient-seamless.pid
+drwxr-xr-x  2 xyp9x xyp9x  4096  5月  7 22:29 Videos
+drwxr-xr-x  2 xyp9x xyp9x  4096  5月  8 17:34 viExam
+-rw-------  1 xyp9x xyp9x 13146  5月  8 17:18 .viminfo
+-rw-r--r--  1 xyp9x xyp9x  1204  5月  8 17:08 .vimrc
+-rwxr-xr-x  1 xyp9x xyp9x   100 10月 30  2017 .Xclients
+-rwxr-xr-x  1 xyp9x xyp9x  1507 11月 29 05:54 .xinitrc
+total 156
+drwx------ 17 xyp9x xyp9x  4096  5月  8 17:18 .
+-rw-------  1 xyp9x xyp9x   739  5月  8 17:04 .bash_history
+-rw-r--r--  1 xyp9x xyp9x    21  1月 13 23:45 .bash_logout
+
